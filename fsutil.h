c8b8638509e738468005990fcf4d368103097d61
xlfeng <xulifeng7@huawei.com>
2021-12-20 17:04:59 +0800

    Modify Huawei license to BSD license.Add Lisence file COPYRIGHT,modify OAT.xml and README.OpenSource.
    Signed-off-by: xlfeng <xulifeng7@huawei.com>

--- fsutil.h
+++ fsutil.h
@@ -1,27 +1,35 @@
 /*
  * Copyright (C) 2021 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdint.h>
 #ifndef _FS_UTIL_H
 #define _FS_UTIL_H
+
+#include <stdint.h>
 
 #define pwarn  printf
 #define pfatal printf
 #define perr   printf
 
-#endif
 
 /*
  * le16dec(),le32dec(),le16enc(),le32enc()
@@ -70,9 +78,10 @@ le32enc(void *pp, uint32_t u)
 /*
  * roundup2
  * Link： https://github.com/freebsd/freebsd-src/
- * Path:  freebsd-src/tools/build/cross-build/include/common/sys/param.h
+ * Path:  freebsd-src/tools/build/cross-build/include/common/sys/cdefs.h
  */
 #ifndef roundup2
 #define roundup2(x, y) \
 	(((x) + ((y)-1)) & (~((y)-1))) /* if y is powers of two */
+#endif
 #endif // _FS_UTIL_H
